829704b09898fa30197a271eac58046f62f9d738
Cabdinasir64 <cabdinasira64@gmail.com>
2025-11-04 16:46:17 +0300

    feat(users): add paginated users route to router
    - added GET /users/paginated route in router
    - connects to getUsersPaginated controller for paginated user retrieval
    - supports page & pageSize query params with pagination metadata

--- src/routes/user.ts
+++ src/routes/user.ts
@@ -1,11 +1,13 @@
 import express from 'express';
-import { getUsers, getUserById, createUser, deleteUser, updateUser, searchUser, filterUsers, countUsers } from '../controllers/user';
+import { getUsers, getUserById, createUser, deleteUser, updateUser, searchUser, filterUsers, countUsers, getUsersPaginated } from '../controllers/user';
 
 const router = express.Router();
 
 router.get('/', getUsers);
 
 router.get('/count', countUsers);
+
+router.get('/paginated', getUsersPaginated);
 
 router.get('/search', searchUser);
 
