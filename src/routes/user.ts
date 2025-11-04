88618bedbf773b431d208e51b65a287302a4466e
Cabdinasir64 <cabdinasira64@gmail.com>
2025-11-04 11:48:37 +0300

    feat(users): add user routes and controller CRUD operations
    - usersController: implement getUsers, getUserById, createUser, updateUser, deleteUser
    - usersRouter: set up routes for all user CRUD endpoints

--- src/routes/user.ts
+++ src/routes/user.ts
@@ -1,9 +1,16 @@
 import express from 'express';
-import { getUsers, createUser } from '../controllers/user';
+import { getUsers, getUserById, createUser, deleteUser, updateUser } from '../controllers/user';
 
 const router = express.Router();
 
 router.get('/', getUsers);
+
+router.get('/:id', getUserById);
+
 router.post('/createusers', createUser);
+
+router.delete('/delete/:id', deleteUser);
+
+router.put("/update/:id", updateUser)
 
 export default router;
