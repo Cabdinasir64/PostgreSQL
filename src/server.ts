32d1d460383c1ccd6064ea64ff8f167530c1dba5
Cabdinasir64 <cabdinasira64@gmail.com>
2025-11-05 14:03:48 +0300

    feat: add role API routes
    - Added /api/roles endpoint
    - Integrated roleRoutes in main server file
    - Supports role CRUD and user-role assignments

--- src/server.ts
+++ src/server.ts
@@ -2,6 +2,7 @@ import express from 'express';
 import dotenv from 'dotenv';
 import userRoutes from './routes/user'
 import usersRoutes from './routes/userRoutes';
+import roleRoutes from './routes/roleRoutes';
 
 dotenv.config();
 const PORT = process.env.PORT;
@@ -13,6 +14,7 @@ app.use(express.json());
 
 app.use('/api/user', userRoutes);
 app.use('/api/users', usersRoutes);
+app.use('/api/roles', roleRoutes);
 
 app.listen(PORT, () => {
     console.log(`🚀 Server running on http://localhost:${PORT}`);
