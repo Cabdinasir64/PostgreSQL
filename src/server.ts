8d3906b20b8290a7d27a479dc0c51b058c635f7a
Cabdinasir64 <cabdinasira64@gmail.com>
2025-11-05 12:20:52 +0300

    feat(routes): add /api/users endpoint with new userRoutes
    - Added `usersRoutes` router for CRUD operations
    - Mounted `/api/users` in main server
    - Keeps `/api/user` for previous routes

--- src/server.ts
+++ src/server.ts
@@ -1,6 +1,7 @@
 import express from 'express';
 import dotenv from 'dotenv';
 import userRoutes from './routes/user'
+import usersRoutes from './routes/userRoutes';
 
 dotenv.config();
 const PORT = process.env.PORT;
@@ -10,7 +11,8 @@ const app = express();
 app.use(express.json());
 
 
-app.use('/api/users', userRoutes);
+app.use('/api/user', userRoutes);
+app.use('/api/users', usersRoutes);
 
 app.listen(PORT, () => {
     console.log(`🚀 Server running on http://localhost:${PORT}`);
