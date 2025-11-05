7824112a18d4f278f3fbdd98e4bd39df070f865c
Cabdinasir64 <cabdinasira64@gmail.com>
2025-11-05 14:32:37 +0300

    feat: integrate user comments API
    - Added /api/comments route to main app
    - Supports CRUD operations on user comments
    - Connected comment routes to comment controller

--- src/server.ts
+++ src/server.ts
@@ -4,6 +4,7 @@ import userRoutes from './routes/user'
 import usersRoutes from './routes/userRoutes';
 import roleRoutes from './routes/roleRoutes';
 import postRoutes from './routes/userPostRoutes';
+import commentRoutes from './routes/userCommentRoutes';
 
 dotenv.config();
 const PORT = process.env.PORT;
@@ -17,6 +18,7 @@ app.use('/api/user', userRoutes);
 app.use('/api/users', usersRoutes);
 app.use('/api/roles', roleRoutes);
 app.use('/api/posts', postRoutes);
+app.use('/api/comments', commentRoutes);
 
 app.listen(PORT, () => {
     console.log(`🚀 Server running on http://localhost:${PORT}`);
